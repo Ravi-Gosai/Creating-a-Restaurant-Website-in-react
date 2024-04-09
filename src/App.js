0c47bde1b620d44d0748a6a95f0d5cb1218ed6cf
Ravi Gosai <rbgosai86@gmail>
2024-04-09 08:09:35 +0530

    summary add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from "./Components/Layout/Header";
 import './App.css'
+import Meals from "./Components/Meals/Meals";
 function App() {
   return (
    <>
    <Header></Header>
+   <main>
+    <Meals></Meals>
+   </main>
    </>
   );
 }
